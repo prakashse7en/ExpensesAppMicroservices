87c4dfdeee91d4b1134cf9d871c4b23ccc442452
prakashse7en <prakash.dec20@gmail.com>
2025-08-12 09:39:32 +0800

    add custom error message for unauthorized token

--- src/main/java/com/digital/transactions/expenses/security/SecurityConfig.java
+++ src/main/java/com/digital/transactions/expenses/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.digital.transactions.expenses.security;
 
+import com.digital.transactions.expenses.aop.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -17,19 +19,24 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+
     private final JwtAuthConverter jwtAuthConverter;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(configurer ->
                         configurer
-                                .requestMatchers("/api/expenses/**"
-                                        )
+                                .requestMatchers("/api/expenses/**")
                                 .authenticated().anyRequest().permitAll())
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
+                                .authenticationEntryPoint(customAuthenticationEntryPoint)
+                )
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(customAuthenticationEntryPoint)
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
         return http.build();
